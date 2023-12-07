a39bbc81b570a95e3f8cd09a2eff59918c92147f
Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2023-12-07 15:49:57 -0500

    Cambios ejercicio 4

--- problematicas/problema03/src/problema03/Problema03.java
+++ problematicas/problema03/src/problema03/Problema03.java
@@ -1,20 +1,22 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
- */
 package problema03;
 
-/**
- *
- * @author LAB.ELECT
- */
 public class Problema03 {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int n=10;
+        for (int a = 1; a < n; a++) {
+            for (int e = 1; e < a; e++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+           for (int a = n-1; a >= 1; a--) {
+            for (int e = 1; e <= a; e++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
     }
-    
+
 }
