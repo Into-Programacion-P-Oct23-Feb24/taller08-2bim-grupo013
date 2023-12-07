7ff2abb50907ce26a75588319e80b7cffcbef229
Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2023-12-07 16:26:25 -0500

    Cambios 3

--- problematicas/problema03/src/problema03/Problema03.java
+++ problematicas/problema03/src/problema03/Problema03.java
@@ -1,17 +1,23 @@
 package problema03;
+
 import java.util.*;
+
 public class Problema03 {
 
     public static void main(String[] args) {
-    Scanner entrada = new Scanner (System.in);
-    double gradosC=0;
-    double gradosF=0;
-    
-    gradosC=5/2*(gradosF-32);
-    
+        Scanner entrada = new Scanner(System.in);
+        double gradosC = 1;
+        double gradosF = 1;
+        String tabla = "Tabla De converciones\n";
+        System.out.print("Grados F: ");
+        gradosF = entrada.nextDouble();
         
-        
-    
+        for (int i = 0; i <= 20; i++) {
+            gradosC = (i - 32.0) * 5 / 9;
+            System.out.println(gradosC);
+            
+        }
+
     }
 
 }
