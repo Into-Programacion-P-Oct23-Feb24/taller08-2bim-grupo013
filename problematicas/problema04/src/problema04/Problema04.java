ba074b28e79d57b35756623eafdd7fd06e044968
Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2023-12-07 16:11:31 -0500

    Cambios ejercicio 3

--- problematicas/problema04/src/problema04/Problema04.java
+++ problematicas/problema04/src/problema04/Problema04.java
@@ -14,7 +14,7 @@ public class Problema04 {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        int n = 10;
+        int n = 7;
         for (int a = 1; a < n; a++) {
             for (int e = 1; e < a; e++) {
                 System.out.print("*");
