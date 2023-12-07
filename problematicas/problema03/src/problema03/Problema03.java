dcab9af269d9585897def06837af699f372f680e
Anthony Joel <romeroyaguanaanthonyjoel@gmail.com>
2023-12-07 16:36:52 -0500

    Q

--- problematicas/problema03/src/problema03/Problema03.java
+++ problematicas/problema03/src/problema03/Problema03.java
@@ -7,16 +7,19 @@ public class Problema03 {
     public static void main(String[] args) {
         Scanner entrada = new Scanner(System.in);
         double gradosC = 1;
-        double gradosF = 1;
+        double gradosF = 20;
         String tabla = "Tabla De converciones\n";
+        int i;
         System.out.print("Grados F: ");
         gradosF = entrada.nextDouble();
-        
-        for (int i = 0; i <= 20; i++) {
+
+        for (i = 1; i < 20; i++) {
+            int e = 1;
+            e++;
             gradosC = (i - 32.0) * 5 / 9;
-            tabla = String.format("", tabla);
-            
+            tabla = String.format("%s%s %s\n", tabla, i, gradosC);
         }
+        System.out.print(tabla);
 
     }
 
