7325d802da504543cec285152f2ce44ee0bb2964
leosaq <leandrosaquisari7@gmail.com>
2023-12-07 17:16:20 -0500

    Problema

--- problematicas/problema03/src/problema03/Problema03.java
+++ problematicas/problema03/src/problema03/Problema03.java
@@ -13,10 +13,11 @@ public class Problema03 {
         int i;
         int e = 20;
         for (i = 1; i < 20; i++) {
-            e+=4;
+            
             gradosC = (e - 32.0) * 5 / 9;
             tabla = String.format("%s      %s                "
                     + "%.2f\n", tabla, e, gradosC);
+            e+=4;
         }
 
         System.out.print(tabla);
